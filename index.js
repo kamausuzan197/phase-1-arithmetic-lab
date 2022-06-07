2961bd877ce21dc722bd28d4afa8bd8b908b8365
kamausuzan197 <kamaususan197@gmail.com>
2022-06-07 07:47:41 +0300

    artimetric work

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 =31 
+let num2 =2 
+let multiply = num1 * num2 
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 =20
+let num4= 5
+let mod = 20/5
+let max = (1, 2, 3, 4, 20);
